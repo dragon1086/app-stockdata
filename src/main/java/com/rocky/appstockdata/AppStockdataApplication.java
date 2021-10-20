69c251029b31620b5b51e7842cac8ec381d3ab84
dragon1086@naver.com <gmrfyd12>
2021-10-20 15:44:54 +0900

    To war deploy, changed some settings

--- src/main/java/com/rocky/appstockdata/AppStockdataApplication.java
+++ src/main/java/com/rocky/appstockdata/AppStockdataApplication.java
@@ -2,9 +2,15 @@ package com.rocky.appstockdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppStockdataApplication {
+public class AppStockdataApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(AppStockdataApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppStockdataApplication.class, args);
