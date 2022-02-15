690b50d53341deee1a1a99ac5b131a3aa8d7d77c
dragon1086@naver.com <gmrfyd12>
2022-02-15 18:47:33 +0900

    open api 고객님 추가

--- src/main/java/com/rocky/appstockdata/domain/validator/OpenApiValidator.java
+++ src/main/java/com/rocky/appstockdata/domain/validator/OpenApiValidator.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class OpenApiValidator {
-    private final static List<String> VALID_ID_LIST = asList("admin");
+    private final static List<String> VALID_ID_LIST = asList("woo2015");
     public static void idValidator(String id) throws NotAllowedException {
         if(!VALID_ID_LIST.contains(id)){
             throw new NotAllowedException("{\"errorMessage\" :  \"허가되지 않은 id입니다.\"}");
