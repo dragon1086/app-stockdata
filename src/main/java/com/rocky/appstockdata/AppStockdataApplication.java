106070f9520f9068356bfeb0051c987b97b2a06d
dragon1086@naver.com <gmrfyd12>
2024-11-04 22:39:35 +0900

    graceful shutdown 개선

--- src/main/java/com/rocky/appstockdata/AppStockdataApplication.java
+++ src/main/java/com/rocky/appstockdata/AppStockdataApplication.java
@@ -17,29 +17,7 @@ public class AppStockdataApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AppStockdataApplication.class);
-
-        // Graceful shutdown 설정
-        app.setRegisterShutdownHook(true);
-
-        // 기본 속성 설정
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("server.shutdown", "graceful");
-        app.setDefaultProperties(properties);
-
-        // 애플리케이션 실행 및 컨텍스트 획득
-        ConfigurableApplicationContext context = app.run(args);
-
-        // Shutdown hook 추가
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                // 진행 중인 요청을 처리할 시간 부여
-                Thread.sleep(2000);
-                context.close();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }));
+        SpringApplication.run(AppStockdataApplication.class, args);
     }
 
 }
